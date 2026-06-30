4ae9ae43a4d8e17806c89a19b97e1e155aecdb41
nunnun-rithy <nunsomrithy098@gmail.com>
2026-06-30 10:26:03 +0700

    dsadsa

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,7 @@ const connectDB = async () => {
   try {
     const mongoUri = process.env.MONGO_URI || "mongodb://aupp-sonarqube-mongo:27017/node_crud";
 
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(mongoUri);
 
     console.log("MongoDB Connected");
   } catch (error) {
